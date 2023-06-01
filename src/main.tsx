5d4bc57c50e70cf67c4c9b8f6c61da12d2f13e6b
It's a me LuisMario <100161156+30ortiz@users.noreply.github.com>
2023-06-01 04:52:46 -0600

    Add Tablas

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.scss';
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <div>Hola</div>
+    <div className='main'>Hola</div>
   </React.StrictMode>,
 )
